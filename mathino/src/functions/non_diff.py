4d9a77246056660bf5c7ffd3954b25e3eec3d84f
kandarpa02 <kandarpaexe@gmail.com>
2026-01-05 21:26:13 +0530

    layers, init

--- mathino/src/functions/non_diff.py
+++ mathino/src/functions/non_diff.py
@@ -1,11 +1,12 @@
-from ..array import as_nd
 from ...backend.backend import xp
 from .utils import unwrap
 
 lib = xp()
 
 def argmax(x, axis=None, keepdims=False):
+    from ..array import as_nd
     return as_nd(lib.argmax(unwrap(x), axis=axis, keepdims=keepdims))
 
 def argmin(x, axis=None, keepdims=False):
+    from ..array import as_nd
     return as_nd(lib.argmin(unwrap(x), axis=axis, keepdims=keepdims))
