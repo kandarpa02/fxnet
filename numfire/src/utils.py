55d4676ec4a545695973e10e45633b678abd1449
kandarpa02 <kandarpaexe@gmail.com>
2026-02-04 20:35:39 +0530

    conv_vjp, linear_vjp

--- numfire/src/utils.py
+++ numfire/src/utils.py
@@ -1,51 +1,8 @@
-import numpy as np
-from typing import Callable
-import torch
+import itertools
+c = 0
 
-def broadcast_backward(grad: torch.Tensor, x_shape: tuple) -> torch.Tensor:
-    # Remove leading dims added by broadcasting
-    from .functions.primitive_reduct import sum
-    while len(grad.shape) > len(x_shape):
-        grad = sum(grad, axis=0)
-
-    # Reduce along broadcasted axes
-    for i, (sx, sg) in enumerate(zip(x_shape, grad.shape)):
-        if sx == 1 and sg != 1:
-            grad = sum(grad, axis=i, keepdims=True)
-
-    return grad
-
-def custom_function(fun):
-    from .base import MakeOP
-    """
-    Define custom function and its gradient rule
-
-    A primitive must return either:
-
-    (out, parents, grad_fn) – explicit parent list
-
-    or:
-
-    (out, grad_fn) – parents inferred as args
-
-    Parameters
-        fun
-        Callable A Python function implementing the forward op. It should NOT mutate inputs and must return a tuple as above.
-
-    Behavior:
-
-        Example primitive:
-
-        @custom_function
-        def add_fun(x, y):
-            out = x + y
-            def grad_fn(g):
-                return g, g
-            return out, (x, y), grad_fn
-
-        Calling:
-
-            z = add_fun(x, y)
-
-    """
-    return MakeOP(fun)
+def var_naming():
+    global c
+    res = c
+    c+=1
+    return f'V:{res}'
